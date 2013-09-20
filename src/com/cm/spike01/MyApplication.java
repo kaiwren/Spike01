79bf0f1bea8b38a553e93e1ef7fc553e456a010c
Sidu Ponnappa <ckponnappa@gmail.com>
2013-09-20 15:34:14 +0530

    Init

---
+++ src/com/cm/spike01/MyApplication.java
@@ -0,0 +1,20 @@
+package com.cm.spike01;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+	public static MyApplication singleton;
+	
+	public static MyApplication getInstance() {
+		return singleton;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		singleton = this;
+	}
+	
+	
+}
